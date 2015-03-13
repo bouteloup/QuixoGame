136dcb47bb0c415d80707d093a9d50298ca33f41
Besland Romain <romain.besland@gmail.com>
2015-03-13 21:40:56 +0100

    update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,29 +10,28 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    //Déclaration bloc croix
+    // Déclaration bloc croix
     QPixmap * image_croix = new QPixmap (":/croix.jpg");
     QGraphicsPixmapItem * item_croix = new QGraphicsPixmapItem(* image_croix);
     QGraphicsScene * scene_croix = new QGraphicsScene();
     scene_croix -> addItem(item_croix);
 
-    //Déclaration bloc rond
+    // Déclaration bloc rond
     QPixmap * image_rond = new QPixmap (":/rond.jpg");
     QGraphicsPixmapItem * item_rond = new QGraphicsPixmapItem(* image_rond);
     QGraphicsScene * scene_rond = new QGraphicsScene();
     scene_rond -> addItem(item_rond);
 
-    //Declaration bloc blanc
+    // Declaration bloc blanc
     QPixmap * coul_blanc = new QPixmap ();
     QGraphicsPixmapItem * bloc_blanc = new QGraphicsPixmapItem(* coul_blanc);
     QGraphicsScene * scene_blanc = new QGraphicsScene();
     scene_blanc -> addItem(bloc_blanc);
 
-    //Répétition des scène
+    // Répétition des scène
     ui->b_1_1->setScene(scene_blanc);
-    ui->b_1_2>setScene(scene_croix);
+    ui->b_1_2->setScene(scene_croix);
     ui->b_1_3->setScene(scene_rond);
-
 }
 MainWindow::~MainWindow()
 {
