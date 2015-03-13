82047e58e020140aeffcec6bdbc2c0845deb5927
Besland Romain <romain.besland@gmail.com>
2015-03-13 21:34:16 +0100

    corrigation de erreur de monsieur bouteloup ip fixe

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,21 +15,23 @@ MainWindow::MainWindow(QWidget *parent) :
     QGraphicsPixmapItem * item_croix = new QGraphicsPixmapItem(* image_croix);
     QGraphicsScene * scene_croix = new QGraphicsScene();
     scene_croix -> addItem(item_croix);
-    ui->ma_vue1->setScene(scene_croix);
 
     //Déclaration bloc rond
     QPixmap * image_rond = new QPixmap (":/rond.jpg");
     QGraphicsPixmapItem * item_rond = new QGraphicsPixmapItem(* image_rond);
     QGraphicsScene * scene_rond = new QGraphicsScene();
     scene_rond -> addItem(item_rond);
-    ui->ma_vue2->setScene(scene_rond);
+
+    //Declaration bloc blanc
+    QPixmap * coul_blanc = new QPixmap ();
+    QGraphicsPixmapItem * bloc_blanc = new QGraphicsPixmapItem(* coul_blanc);
+    QGraphicsScene * scene_blanc = new QGraphicsScene();
+    scene_blanc -> addItem(bloc_blanc);
 
     //Répétition des scène
-    scene_croix -> addItem(item_croix);
-    ui->ma_vue3->setScene(scene_croix);
-
-    scene_rond -> addItem(item_rond);
-    ui->ma_vue3->setScene(scene_rond);
+    ui->b_1_1->setScene(scene_blanc);
+    ui->b_1_2>setScene(scene_croix);
+    ui->b_1_3->setScene(scene_rond);
 
 }
 MainWindow::~MainWindow()
